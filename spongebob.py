edf03a724bf30d2035dc16f9c3d15dbb6c5b0f0f
djvilas <djvilas@gmail.com>
2019-01-29 12:52:16 +0000

    Update spongebob.py

--- spongebob.py
+++ spongebob.py
@@ -1,17 +1,20 @@
 def spongebob():
   #Inspired by Fiona Tuite, Third Year Computer Applications & Software Engineering student at Dublin City University, 2019.
   import random 
-  text = input()
+  text = input("Input the word/s that you'd like to alter: ")
   finish = 0
   while finish == 0:
-    newText = ""
-    for x in text:
-      if random.randint(0,2) > 1:
-        newText += x.capitalize()
-      else:
-        newText += x
+    updatedText = text
+    for x in range(0,10):
+      newText = ""
+      for x in updatedText:
+        if random.randint(0,2) > 1:
+          newText += x.upper()
+        else:
+          newText += x.lower()
+      updatedText = newText    
     
-    print("We've generated:\n"+newText+"\n Would you like to generate a new version?")
+    print("\nWe've generated:\n"+updatedText+"\nWould you like to generate a new version?\n")
     choice = input("Generate Again? Yes|No : ")
     if choice.lower() == "yes":
       choice = 0
@@ -19,7 +22,6 @@ def spongebob():
       break
     else:
       print(choice.lower())
-      print("We didn't understand your command... Generating again.")    
+      print("\nWe didn't understand your command... Generating again.\n")    
      
-
 spongebob()
